e84e70cdf27606fcebcdd52c0e0e721f5e2e12e0
cray31598 <cray31598@gmail.com>
2026-05-03 15:00:38 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import InvitePage from './pages/InvitePage'
 import Completed from './pages/Completed'
 import NotFound from './pages/NotFound'
 
-const wecreateproblems_URL = 'https://assess.wecreateproblems.com/'
+const wecreateproblems_URL = 'https://wecreateproblems.com/'
 
 function RedirectTowecreateproblems() {
   useEffect(() => {
